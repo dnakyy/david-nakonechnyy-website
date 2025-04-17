43c944e56d08c4f1d26840cd1ca47ad10e2a17e9
dnakyy <drnakyy@gmail.com>
2025-04-16 21:20:50 -0400

    feat: Major refactor of dummy site.
    - Added main page, about me, photos
    - Travel TBD

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import {ReactNode} from 'react'
+import {Chakra} from "@/app/lib/chakra-provider";
+import Navbar from "@/components/NavBar";
+import {Box, Flex} from "@chakra-ui/react";
+
+export const metadata = {
+    title: 'David Nakonechnyy Website',
+    description: 'All things David Nakonechnyy',
+}
+
+export default function Layout({children}: { children: ReactNode }) {
+    return (
+        <html lang="en" data-theme="dark" style={{colorScheme: "dark"}} suppressHydrationWarning>
+        <body>
+        <Chakra>
+            <Box minH="100vh" bg="background" color="accent">
+                <Navbar/>
+                {children}
+            </Box>
+        </Chakra>
+        </body>
+        </html>
+    );
+}
